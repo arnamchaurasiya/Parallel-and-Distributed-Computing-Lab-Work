1bad9155615ca9f5b90eb16a0c45c505ba558fa8
Arnam <achaurasiya_be22@thapar.edu>
2025-03-18 16:48:45 +0530

    added folder

--- Assignment_1/mpi_hello_world.c
+++ Assignment_1/mpi_hello_world.c
@@ -14,7 +14,7 @@ int main(int argc, char** argv) {
     int name_len;
     MPI_Get_processor_name(processor_name, &name_len);
     
-    printf("Hello world from processor %s, rank %d out of %d processors \n", processor_name, world_rank, world_size);
+    printf("Hello Duniya from processor %s, rank %d out of %d processors \n", processor_name, world_rank, world_size);
     
     MPI_Finalize();
 }
